a8f30eddd9c92a38cc745f7bc631c4f42c502502
Haydon Curteis-Lateo <haydon1900@gmail.com>
2019-02-16 17:39:37 +0000

    InfJavascriptGame

---
+++ games/InfGame/js/scripts.js
@@ -0,0 +1,157 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth /1.3;
+ctx.canvas.height = window.innerHeight /1.5;
+var player = {x:50,y:canvas.height-500,r:25,yv:0,jumping:false,colliding:false,pointGained:false};
+var container = {x:0,y:0,width:canvas.width,height:canvas.height - 25};
+var floor = {x:0,y:canvas.height - 25,width:canvas.width,height:25};
+var barrier = {x:canvas.width + 50,y:canvas.height - 150,width:50,height:125,xv:0};
+var score = 0;
+var scoreTXT = document.getElementById('scoreTXT');
+
+
+controller = {
+    up:false,
+    keyListener:function(e) {
+        var key_state = (e.type == "keydown")?true:false;
+        switch(e.keyCode) {
+            //Spacebar
+            case 32:
+                e.preventDefault();
+                controller.up = key_state;
+            break;
+            //Up arrow
+            case 38:
+                e.preventDefault();
+                controller.up = key_state;
+            break;
+            //w key
+            case 87:
+                e.preventDefault();
+                controller.up = key_state;
+            break;
+        }
+    }
+};
+
+function containerDraw(){
+    if (player.colliding) {
+        ctx.fillStyle = "#c60731";
+        ctx.fillRect(container.x, container.y, container.width, container.height);
+    }
+    else {
+        ctx.fillStyle = "#52c1ea";
+        ctx.fillRect(container.x, container.y, container.width, container.height);
+    }
+
+}
+
+function floorDraw() {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(floor.x, floor.y, floor.width, floor.height);
+}
+
+function playerDraw() {
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, player.r, 0, Math.PI * 2, false);
+    ctx.fillStyle = '#ffffff';
+    ctx.fill();
+}
+function barrierDraw() {
+    ctx.fillStyle = "#514b33";
+    ctx.fillRect(barrier.x, barrier.y, barrier.width, barrier.height);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+    containerDraw();
+    floorDraw();
+    barrierDraw();
+    playerDraw();
+
+    //move player down
+    player.yv += 0.6; //gravity
+    player.y += player.yv;
+    player.yv *= 0.9;
+
+    //stop player moving down
+    if (player.y + player.r > floor.y) {
+        player.jumping = false;
+        player.y = floor.y - player.r;
+        player.yv = 0;
+    }
+
+    //move player up
+    if (controller.up && player.jumping == false) {
+        player.yv -=40;
+        player.jumping = true;
+    }
+
+    //move barrier left
+    barrier.xv += 0.5;
+    barrier.x -= barrier.xv;
+    barrier.xv *= 0.9;
+
+    //reset barrier when it hits edge
+    if (barrier.x  + barrier.width < 0) {
+        barrier.x = canvas.width + barrier.width;
+    }
+
+    // add score
+    if ((Math.ceil((player.x + player.r) / 10) * 10) === (Math.ceil((barrier.x + barrier.width) / 10) * 10) && !collisionDetection(player, barrier)) {
+        if (!player.pointGained) {
+            score += 1;
+            scoreTXT.textContent = score;
+            player.pointGained = true;
+        }
+    }else {
+        player.pointGained = false;
+    }
+    // console.log(Math.ceil(barrier.x + barrier.width / 10) * 10);
+
+    //reset score when player hits barrier
+    if (collisionDetection(player, barrier)) {
+        player.colliding = true;
+        score = 0;
+        scoreTXT.textContent = score;
+    }
+    else {
+        player.colliding = false;
+    }
+
+    window.requestAnimationFrame(draw, canvas);
+}
+
+draw();
+
+
+function collisionDetection(player, barrier) {
+    var distX = Math.abs(player.x - barrier.x-barrier.width/2);
+    var distY = Math.abs(player.y - barrier.y-barrier.height/2);
+
+    if (distX > (barrier.width/2 + player.r)) { return false; }
+    if (distY > (barrier.height/2 + player.r)) { return false; }
+
+    if (distX <= (barrier.width/2)) { return true; }
+    if (distY <= (barrier.height/2)) { return true; }
+
+    var dx = distX-barrier.width/2;
+    var dy = distY-barrier.height/2;
+    return (dx*dx+dy<=(player.r*player.r));
+}
+
+
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(draw, canvas);
+
+if (!window.requestAnimationFrame) { // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+    window.requestAnimationFrame = window.webkitRequestAnimationFrame || 
+                                   window.mozRequestAnimationFrame    || 
+                                   window.oRequestAnimationFrame      || 
+                                   window.msRequestAnimationFrame     || 
+                                   function(callback, element) {
+                                     window.setTimeout(function() { callback(Date.now()); }, 1000 / 60);
+                                   }
+  }
+  
